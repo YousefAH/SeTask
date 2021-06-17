c3e9d1a7f56402c24932f34e0d5a50683fb3e408
Mohamed Ealyadi <ayadi@MacBook-Pro-Mohamed.local>
2021-06-17 14:01:00 +0200

    added date

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
-from datetime import datetime
+from datetime import datetime, date
 now = datetime.now()
+today = date.today()
+current_date = today.strftime("%d/%m/%Y")
 current_time = now.strftime("%H:%M:%S")
 print("Current Time: ", current_time)
+print("Current Date: ", current_date)
+
